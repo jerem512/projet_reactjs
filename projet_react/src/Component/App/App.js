741c395c6529c8d25ab76f305aa69be652da6042
jeremy <jeremymarchal90@gmail.com>
2019-11-27 17:53:51 +0100

    Affichage principal

--- projet_react/src/Component/App/App.js
+++ projet_react/src/Component/App/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <p> test2 </p>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {addClass: false}
+    }
+    toggle() {
+        this.setState({addClass: !this.state.addClass});
+    }
+    render() {
 
+        let boxClass = ["box"];
+        if(this.state.addClass) {
+            boxClass.push('hide');
+        }
+        let divClass = ["backdiv"];
+        if(this.state.addClass) {
+            divClass.push('hide');
+        }
+        let pClass = ["paragraphe"];
+        if(this.state.addClass) {
+            pClass.push('show');
+        }
+        return(
+
+
+            <div>
+                <div  className={ divClass.join(' ')} onClick={this.toggle.bind(this)}>
+                    <div className={ boxClass.join(' ')} onClick={this.toggle.bind(this)}>
+                        <p className="jouer">JOUER</p>
+                    </div>
+                </div>
+                <div>
+                    <p>code de Andrei</p>
+                </div>
+            </div>
+
+        );
+
+    }
+}
 export default App;
