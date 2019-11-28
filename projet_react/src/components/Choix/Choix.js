4b4f11baf731b209426620a89fc58bdd66cf8f05
Arnaud <arnaudpadula5@gmail.com>
2019-11-28 14:35:38 +0100

    FrontComplet

--- projet_react/src/components/Choix/Choix.js
+++ projet_react/src/components/Choix/Choix.js
@@ -1,40 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import icoAudio from './media/icoAudio.svg';
+import icoNoAudio from './media/icoNoAudio.svg';
+
 import './Choix.css'
 
-let emotions = ['emo1', 'emo2', 'emo3', 'emo4'];
-let soundFragments = ['soundEx1', 'soundEx2', 'soundEx3', 'soundEx4', 'soundEx5', 'soundEx6', 'soundEx7', 'soundEx8', 'soundEx9']
-    
-let soundIco = {
-    icoSrcON: './media/icoAudio.png',
-    icoSrcOFF: './media/icoNoAudio.png'
+let emotions = [
+    "emo1",
+    "emo2",
+    "emo3",
+    "emo4"
+];
+
+function randArr(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function randomEmotions() {
-    return emotions[Math.floor(Math.random() * emotions.length)]
-}
-
-function randomSound() {
-    return soundFragments[Math.floor(Math.random() * soundFragments.length)]
+    return randArr(emotions);
 }
 
 class Choix extends React.Component {
     
     constructor(props) {
         super(props);
-        this.emotion = [null, null];
+        this.emotions = [null, null];
         this.soundFragment = null;
     }
 
     generateEmo() {
-        this.emotion = [randomEmotions(), randomEmotions()];
-        while(this.emotion[0] == this.emotion[1]) {
-            this.emotion[1] = randomEmotions();
+        this.emotions = [randomEmotions(), randomEmotions()];
+        while(this.emotions[0] == this.emotions[1]) {
+            this.emotions[1] = randomEmotions();
         }
     }
 
     generateSound() {
-        this.soundFragment = randomSound();
+        this.soundFragment = Math.floor(Math.random()*2);
     }
 
     updateState() {
@@ -44,8 +47,26 @@ class Choix extends React.Component {
         });
     }
 
-    getSndFrg() {
-        return this.soundFragment;
+    launch() {
+        this.generateEmo();
+        this.generateSound();
+        this.updateState();
+    }
+
+    start() {
+        this.launch();
+        let thisdom = ReactDOM.findDOMNode(this);
+        thisdom.querySelector("#arrowPlay").classList.add("hide");
+        thisdom.querySelector("#play").classList.remove("hide");
+    }
+
+    validate(i) {
+        if(i == this.soundFragment) {
+            alert("YAY");
+        } else {
+            alert("NAH");
+        }
+        this.launch();
     }
 
     render() {
@@ -53,26 +74,24 @@ class Choix extends React.Component {
             <div id="choix_gen_js" className="d-flex flex-row justify-content-around mr-auto ml-auto">
                 <div>
                     <h2>Réponse une</h2>
-                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block mr-5">
-                        <p className="BscEmo">{this.emotion[0]}</p>
-                        <p className="CpxEmo"></p>
+                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block mr-5" onClick={() => this.validate(0)}>
+                        <p className="emo">{this.emotions[0]}</p>
                     </button>
                 </div>
                 <div>
                     <h2>Réponse deux</h2>
-                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block">
-                        <p className="BscEmo">{this.emotion[1]}</p>
-                        <p className="CpxEmo"></p>
+                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block" onClick={() => this.validate(1)}>
+                        <p className="emo">{this.emotions[1]}</p>
                     </button>
                 </div>
                 
                 <div id="functions" className="d-flex flex-column mr-auto ml-auto">
-                <button type="button" id="playButt" className="btn btn-primary btn-lg btn-block" onClick={() => {this.generateEmo(); this.generateSound(); this.updateState();}}>Jouer</button>
-                <div id="soundLogo">
-                    <img src={soundIco.icoSrcON} alt="logo"/>
-                    <div>{"Hello world " + this.soundFragment}</div>
+                    <a href="#" id="arrowPlay" className="btn btn-info btn-lg" onClick={() => this.start()}><span className="glyphicon glyphicon-play"></span>Commencez</a>
+                    <div id="play" className="hide mr-auto ml-auto">
+                        <img src={icoAudio} alt="logo"/>
+                        <div>{this.soundFragment}</div>
+                    </div>
                 </div>
-            </div>
             </div>
             
         )
