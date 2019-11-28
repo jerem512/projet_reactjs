53bf18b42235d0f7bcb3a12c4ffe6e5e094bb9e6
Florian <54706483+Florian-bit@users.noreply.github.com>
2019-11-28 12:20:26 +0100

    Score + multiplicateur

--- projet_react/src/Component/Score/Score.js
+++ projet_react/src/Component/Score/Score.js
@@ -2,32 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Score.css';
 
-
 class Compteur extends React.Component {
+    
     constructor() {
         super()
         this.state = {
             nb: 0,
         }
+        this.multiplier = 1;
+        this.combo = 0;
     }
- 
+
     increment() {
+        /*if(this.multiplier < 8) {
+            this.combo ++;
+            if(this.combo == 2) {
+                this.combo = 0;
+                this.multiplier *= 2;
+            }
+        }*/
+        this.combo ++;
+        this.multiplier = Math.pow(2, Math.min(this.combo - 1, 3));
+        //this.multiplier = Math.pow(2, this.combo - 1);
         this.setState({
-            nb: this.state.nb + 1
-        })
+            nb: this.state.nb + 1 * this.multiplier    
+        });
     }
-    
+
     decrement() {
         this.setState({
             nb: this.state.nb - 1
         })
+        this.combo = 0;
+        this.multiplier = 1;
     }
 
     render() {
         return (
             <div className="score_compteur">
                 <p>   
-                Score : {this.state.nb}
+                Score(x{this.multiplier}) : {this.state.nb}
                 </p>
                 <button className="btn btn-warning" onClick={ ( ) => this.increment() }>
                     +1
