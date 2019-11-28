4ab6d74351a8c25e784e7ff2b04acb1e2c6c28e7
Arnaud <arnaudpadula5@gmail.com>
2019-11-28 09:33:49 +0100

    RandomEmotions

--- projet_react/src/components/Choix/Choix.js
+++ projet_react/src/components/Choix/Choix.js
@@ -2,90 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Choix.css'
 
-let emotions = {
-    emo1: {
-        name: 'emo1',
-        complEmo1: ['1.1', '1.2']
-    },
-    emo2: {
-        name: 'emo2',
-        complEmo2: ['2.1', '2.2']
-    },
-    emo3: {
-        name: 'emo3',
-        complEmo3: ['3.1', '3.2']
-    },
-    emo4: {
-        name: 'emo4',
-        complEmo4: ['4.1', '4.2']
-    }
-}
-let choix1 = { name: 'Option 1' }
-let choix2 = { name: 'Option 2' }
-let sound = {
+let emotions = ['emo1', 'emo2', 'emo3', 'emo4'];
+    
+let soundIco = {
     icoSrcON: './media/icoAudio.png',
     icoSrcOFF: './media/icoNoAudio.png'
 }
+
 let play = {
     playON: './media/icoPlay.png',
     playOFF: './media/icoStop.png'
 }
 
-class Choix1 extends React.Component {
+let game;
+
+function randomEmotions() {
+    return emotions[Math.floor(Math.random()*emotions.length)]
+}
+
+class Choix extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            emotions: [null, null]
+        };
+        game = this;
+    }
+
+    generate() {
+        let emotions = [randomEmotions(), randomEmotions()];
+        while(emotions[0] == emotions[1]) {
+            emotions[1] = randomEmotions();
+        }
+        this.setState({emotions: emotions});
+    }
+
     render() {
         return (
-            <div className="Choix">
-                <h2>{choix1.name}</h2>
-                <button type="button" className="div_choix btn btn-primary btn-lg btn-block">
-                    <p className="BasicEmo">{emotions.emo1.name}</p>
-                    <p className="ComplEmo">{emotions.emo1.complEmo1[0]}</p>
-                </button>
+            <div id="choix_gen_js" className="d-flex flex-row justify-content-around mr-auto ml-auto">
+                <div>
+                    <h2>Option 1</h2>
+                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block mr-5">
+                        <p className="BscEmo">{this.state.emotions[0]}</p>
+                        <p className="CpxEmo"></p>
+                    </button>
+                </div>
+                <div>
+                    <h2>Option 2</h2>
+                    <button type="button" className="choix_sec_js btn btn-primary btn-lg btn-block">
+                        <p className="BscEmo">{this.state.emotions[1]}</p>
+                        <p className="CpxEmo"></p>
+                    </button>
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+class Audio_Play_js extends React.Component {
+    render() {
+        return (
+            <div id="functions" className="d-flex flex-column mr-auto ml-auto">
+                <button type="button" id="playButt" className="btn btn-primary btn-lg btn-block" onClick={() => game.generate()}>Jouer</button>
+                <div id="soundLogo">
+                    <img src={soundIco.icoSrcON} alt="logo"/>
+                </div>
             </div>
         )
     }
 }
 
-class Sound extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={sound.icoSrcON}/>
-            </div>
-        )
-    }
-}
 
-class Play extends React.Component {
-    render() {
-        return <button type="button" className="div_play btn btn-primary btn-lg btn-block">Jouer</button>
-    }
-}
+ReactDOM.render(<Choix/>, document.getElementById('choix_html'));
+ReactDOM.render(<Audio_Play_js/>, document.getElementById('audio_play_html'));
 
-class Choix2 extends React.Component {
-    render() {
-        return (
-            <div className="Choix">
-                <h2>{choix2.name}</h2>
-                <button type="button" className="div_choix btn btn-primary btn-lg btn-block">
-                    <p className="BasicEmo">{emotions.emo2.name}</p>
-                    <p className="ComplEmo">{emotions.emo2.complEmo2[1]}</p>
-                </button>
-            </div>
-        )
-    }
-}
-
-class Valider extends React.Component {
-    render() {
-        return <button type="button" className="div_valider btn btn-primary btn-lg btn-block">Valider</button>
-    }
-}
-
-ReactDOM.render(<Choix1/>, document.getElementById('div_choix1'));
-ReactDOM.render(<Choix2/>, document.getElementById('div_choix2'));
-//ReactDOM.render(<Sound/>, document.getElementById('soundLogo'));
-ReactDOM.render(<Play/>, document.getElementById('play'));
-ReactDOM.render(<Valider/>, document.getElementById('valider'))
 
 /*
 let valider = {
