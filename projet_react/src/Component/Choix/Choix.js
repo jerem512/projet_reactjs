0726b9a021c24643b78f4aae73a6cee922b3b292
jeremy <jeremymarchal90@gmail.com>
2019-12-03 14:47:28 +0100

    last fix, complete game

--- projet_react/src/Component/Choix/Choix.js
+++ projet_react/src/Component/Choix/Choix.js
@@ -10,14 +10,47 @@ import tristesse from "../../audio/tristesse.ogg";
 
 import './Choix.css';
 
+class Emotion {
+
+    constructor(name, audio, color/*, desc*/) {
+        this.name = name;
+        this.audio = audio;
+        this.color = color;
+        this.desc = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+        this.played = false;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getAudio() {
+        this.played = true;
+        return this.audio;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    getDesc() {
+        return this.desc;
+    }
+
+    hasPlayed() {
+        return this.played;
+    }
+
+}
+
 let emotions = [
-    ["Colère", colere, "red"],
-    ["Tristesse", tristesse, "lime"],
-    ["Joie", joie, "blue"],
-    ["Peur", peur, "purple"]
+    new Emotion("Colère", colere, "red"),
+    new Emotion("Tristesse", tristesse, "lime"),
+    new Emotion("Joie", joie, "blue"),
+    new Emotion("Peur", peur, "purple")
 ];
 
-let dummyEmotion = ["null", "null", "black"];
+let dummyEmotion = new Emotion("", null, "black");
 
 function randArr(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -28,15 +61,15 @@ function randomEmotions() {
 }
 
 export class Choix extends React.Component {
-    
-	static INSTANCE;
-	
+
+    static INSTANCE;
+
     constructor(props) {
         super(props);
         this.emotions = [dummyEmotion, dummyEmotion];
         this.soundFragment = null;
-		this.enPAuse = false;
-		Choix.INSTANCE = this;
+        this.enPAuse = false;
+        Choix.INSTANCE = this;
     }
 
     generateEmo() {
@@ -47,11 +80,24 @@ export class Choix extends React.Component {
     }
 
     generateSound() {
-        this.soundFragment = Math.floor(Math.random()*2);
-		let player = ReactDOM.findDOMNode(this).querySelector("#emotionPlayer");
-		player.setAttribute("src", this.emotions[this.soundFragment][1]);
-		player.play();
-		Timer.INSTANCE.freeze();
+        let canContinue = false;
+        for(let emotion of emotions) {
+            if(!emotion.hasPlayed()) {
+                canContinue = true;
+            }
+        }
+
+        if(canContinue) {
+            this.soundFragment = Math.floor(Math.random()*2);
+            let player = ReactDOM.findDOMNode(this).querySelector("#emotionPlayer");
+            player.setAttribute("src", this.emotions[this.soundFragment].getAudio());
+            player.play();
+        } else {
+            //console.log("%cGG", "color: red; font-size: 10em;");
+            document.querySelector("#selectText").classList.add("hide");
+            document.querySelector("#endText").classList.remove("hide");
+        }
+        Timer.INSTANCE.freeze();
     }
 
     updateState() {
@@ -70,17 +116,15 @@ export class Choix extends React.Component {
     start() {
         this.launch();
         let thisdom = ReactDOM.findDOMNode(this);
-        let thisbody = document.body;
         thisdom.querySelector("#arrowPlay").classList.add("hide");
         thisdom.querySelector("#play").classList.remove("hide");
         thisdom.querySelector("#choix_gen_js").classList.remove("hide");
         thisdom.querySelector("#mid").classList.add("hide");
-        thisbody.querySelector("#rules").classList.add("hide");
-        thisbody.querySelector("#txtrgl").classList.add("hide");
-        thisbody.querySelector("#controls").classList.remove("hide");
+        document.body.querySelector("#rules").classList.add("hide");
+        document.body.querySelector("#txtrgl").classList.add("hide");
+        document.body.querySelector("#controls").classList.remove("hide");
+        Timer.INSTANCE.init();
 
-		Timer.INSTANCE.init();
-		
     }
 
     validate(i) {
@@ -103,23 +147,24 @@ export class Choix extends React.Component {
             for(let btn of document.body.querySelectorAll("button.btn")) {
                 btn.blur();
             }
-			Timer.INSTANCE.reset();
+            Timer.INSTANCE.reset();
             Timer.INSTANCE.stop();
             this.launch();
         }
     }
-	
-	audioEnd() {
-		Timer.INSTANCE.unfreeze();
-		Timer.INSTANCE.start();
-	}
 
-	audioRestart(){
+    audioEnd() {
+        Timer.INSTANCE.unfreeze();
+        Timer.INSTANCE.start();
+    }
+
+    audioRestart() {
         if(!Timer.INSTANCE.isPaused()) {
             document.getElementById("emotionPlayer").play();
         }
     }
-    definition(){
+
+    definition() {
         let help  = document.querySelector("#help");
         help.classList.remove("hide");
         help.classList.add("divdef");
@@ -127,7 +172,8 @@ export class Choix extends React.Component {
         Timer.INSTANCE.stop();
         Timer.INSTANCE.freeze();
     }
-    exitDefinition(){
+
+    exitDefinition() {
         let exit = document.querySelector("#help");
         exit.classList.add("hide");
         Timer.INSTANCE.unfreeze();
@@ -144,12 +190,12 @@ export class Choix extends React.Component {
                     <div id="help" className="hide">
                         <button onClick={()=>this.exitDefinition()} className="btn exit m-2">x</button>
                         <div className="text-warning p-2">
-                            <legend>{this.emotions[0][0]} :</legend>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque culpa cum deleniti doloribus enim, hic itaque iusto laboriosam mollitia natus neque porro praesentium soluta tempore velit voluptatem voluptates. Voluptatum?</p>
+                            <legend>{this.emotions[0].getName()} :</legend>
+                            <p>{this.emotions[0].getDesc()}</p>
                         </div>
                         <div className="text-warning p-2">
-                            <legend>{this.emotions[1][0]} :</legend>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque culpa cum deleniti doloribus enim, hic itaque iusto laboriosam mollitia natus neque porro praesentium soluta tempore velit voluptatem voluptates. Voluptatum?</p>
+                            <legend>{this.emotions[1].getName()} :</legend>
+                            <p>{this.emotions[1].getDesc()}</p>
                         </div>
                     </div>
                     <legend id="txtrgl" className="mx-auto text-danger txtrgl">! Veuillez lire les règles du jeu avant de commencer !</legend>
@@ -161,30 +207,26 @@ export class Choix extends React.Component {
                         </div>
                     </div>
                     <div id="choix_gen_js" className="hide">
-                        <h2 className="text-center mt-5">Sélectionnez une réponse (une seule est correcte).</h2>
+                        <h2 id="selectText" className="text-center mt-5">Sélectionnez une réponse (une seule est correcte).</h2>
+                        <h2 id="endText" className="text-center mt-5 hide">Fin du jeu!</h2>
                         <p id="answer" className="invisible">Aucune réponse</p>
                         <div className="firstdivdef my-2">
                             <button className="btn text-center def" onClick={(this.definition)}>Définitions</button>
                         </div>
                         <div className="d-flex justify-content-center">
-                            <button type="button" className="btn bouton mx-3" style={{backgroundColor: this.emotions[0][2]}} onClick={() => this.validate(0)}>
-                                <p className="emo">{this.emotions[0][0]}</p>
+                            <button type="button" className="btn bouton mx-3" style={{backgroundColor: this.emotions[0].getColor()}} onClick={() => this.validate(0)}>
+                                <p className="emo">{this.emotions[0].getName()}</p>
                             </button>
-                            <button type="button" className="btn bouton mx-3" style={{backgroundColor: this.emotions[1][2]}} onClick={() => this.validate(1)}>
-                                <p className="emo">{this.emotions[1][0]}</p>
+                            <button type="button" className="btn bouton mx-3" style={{backgroundColor: this.emotions[1].getColor()}} onClick={() => this.validate(1)}>
+                                <p className="emo">{this.emotions[1].getName()}</p>
                             </button>
                         </div>
                     </div>
                 </div>
-				<audio id="emotionPlayer" onEnded={this.audioEnd}></audio>
+                <audio id="emotionPlayer" onEnded={this.audioEnd}></audio>
             </div>
-
         )
     }
 }
-
 ReactDOM.render(<Choix/>, document.getElementById('choix_html'));
-
 export default  Choix;
-
-
